798d4ed180bacbd3b31c93a6472e83279e48d956
BaronLip <baronlip@gmail.com>
2019-11-15 10:39:28 -0600

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -3,36 +3,38 @@ import Cell from './cell.js';
 
 export default class Matrix extends Component {
     
-    genRow = (vals) => (
-        // console.log(vals)
-        vals.map(val => (<Cell rowVals={val} />)) // replace me and render a cell component instead!
-    )
+    genRow = (vals) => {
+        //console.log(vals)
+        return vals.map(val => <Cell value={val} />) // replace me and render a cell component instead!
+    }
     
-    genMatrix = () => (
-        // console.log(this.props)
-        this.props.values.map(rowVals => 
-            <div className="row">{this.genRow(rowVals)}</div>)
-    )
+    genMatrix = () => {
+        //console.log(this.props.values)
+        return this.props.values.map(
+            rowVals => <div className="row">{this.genRow(rowVals)}</div>
+        )
+    }
     
     render() {
         // console.log(this.props.values) // This part works.
         return (
             <div id="matrix">
-                {this.genMatrix()}
+            {this.genMatrix()}
+            
             </div>
-        )
+            )
+        }
     }
-}
 
-Matrix.values = [
-    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
-    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
-    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
-    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
-    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
-    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
-    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
-    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
-    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
-    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F']
-]
+    Matrix.defaultProps = {
+    values: [['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']]
+}
--- src/cell.js
+++ src/cell.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-    // constructor(props) {
-    //     super() //super() is required, need to find out why.
-    //     this.state = {
-    //         color: prop
-    //     }
-    // }
+    
+    constructor(props) {
+        
+        super() //super() is required, need to find out why.
+        this.state = {
+            color: props.value
+        }
+    }
 
-    // render() {
-    //     return {
-    //         <div className="cell" style={{ backgroundColor: this.state.color }}></div>
-    //     }
-    // }
+    handleClick = () => {
+        this.setState({
+            color: "#333"
+            }
+        )
+    }
+
+    render() {
+        console.log(this.props)
+        return (
+            <div 
+                className="cell" 
+                style={{backgroundColor: this.state.color}}
+                onClick={ this.handleClick }>
+            </div>
+        )
+    }
 }
