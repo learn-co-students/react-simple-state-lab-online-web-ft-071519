410091a00565ee69cf9679c376dd5a8fcad50fa4
BaronLip <baronlip@gmail.com>
2019-11-14 21:39:57 -0600

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import Cell from './cell.js';
 
 export default class Matrix extends Component {
-  
-  genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  )
-  
-  genMatrix = () => (
-    this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  )
-  
-  render() {
-    return (
-      <div id="matrix">
-        {this.genMatrix()}
-      </div>
+    
+    genRow = (vals) => (
+        // console.log(vals)
+        vals.map(val => (<Cell rowVals={val} />)) // replace me and render a cell component instead!
     )
-  }
-  
+    
+    genMatrix = () => (
+        // console.log(this.props)
+        this.props.values.map(rowVals => 
+            <div className="row">{this.genRow(rowVals)}</div>)
+    )
+    
+    render() {
+        // console.log(this.props.values) // This part works.
+        return (
+            <div id="matrix">
+                {this.genMatrix()}
+            </div>
+        )
+    }
 }
+
+Matrix.values = [
+    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+    ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F']
+]
---
+++ src/cell.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+    // constructor(props) {
+    //     super() //super() is required, need to find out why.
+    //     this.state = {
+    //         color: prop
+    //     }
+    // }
+
+    // render() {
+    //     return {
+    //         <div className="cell" style={{ backgroundColor: this.state.color }}></div>
+    //     }
+    // }
+}
